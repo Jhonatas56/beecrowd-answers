5c06db76b775fc34b30e7b0a4dcf4e944e89bbdb
Jhonatas J. Mendonça <118845977+Jhonatas56@users.noreply.github.com>
2024-04-13 12:10:27 -0300

    add 1003

---
+++ 1 - Iniciante/1003 - Soma Simples.js
@@ -0,0 +1,15 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+ 
+const a = parseInt(lines.shift());
+const b = parseInt(lines.shift());
+
+const soma = a+b;
+
+console.log("SOMA = " + soma);
